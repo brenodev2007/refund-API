81e1d9d792ff601671854aee2f00454250507cdf
Breno Soriani <breno.soriani@gmail.com>
2025-07-11 16:41:48 -0300

    Validação de dados da solicitação

--- src/controllers/RefundController.ts
+++ src/controllers/RefundController.ts
@@ -1,8 +1,28 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "acommodation",
+]);
 
 export class RefundController {
   create = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const bodySchema = z.object({
+        name: z.string().trim(),
+        category: CategoriesEnum,
+        amount: z
+          .number()
+          .positive({ message: "o numero precisa ser positivo" }),
+        filename: z.string(),
+      });
+
+      const { name, category, amount, filename } = bodySchema.parse(req.body);
+      res.json({ name, category, amount, filename });
     } catch (error) {
       next(error);
     }
