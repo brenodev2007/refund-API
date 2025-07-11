f5e040bf37cd5cd5046b863f69fa9bf350735140
Breno Soriani <breno.soriani@gmail.com>
2025-07-11 15:28:27 -0300

    Validando dados da sessão

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,8 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 
 export class SessionController {
   create = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const bodySchema = z.object({
+        email: z.string().email({ message: "o email é obrigatório" }),
+        password: z.string(),
+      });
+
+      const { email, password } = bodySchema.parse(req.body);
+
+      res.json({ email, password });
     } catch (error) {
       next(error);
     }
