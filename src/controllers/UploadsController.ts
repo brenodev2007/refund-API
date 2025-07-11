5121cb2e103b75c81cf06f4aa6356cdc7d5dfac8
Breno Soriani <breno.soriani@gmail.com>
2025-07-11 17:45:45 -0300

    Validando arquivo

--- src/controllers/UploadsController.ts
+++ src/controllers/UploadsController.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 
 export class UploadsController {
   create = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.json({ message: "ok" });
+      const fileSchema = z.object({
+        filename: z.string().min(1),
+        mimetype: z.string(),
+        size: z.number().positive(),
+      });
+
+      const { filename, mimetype, size } = fileSchema.parse(req.file);
+      res.json();
     } catch (error) {
       next(error);
     }
