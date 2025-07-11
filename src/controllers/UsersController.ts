baa45963a23d0f1c3023a5039f012904bce6bf4e
Breno Soriani <breno.soriani@gmail.com>
2025-07-11 15:10:52 -0300

    Validando dados do usuário

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,9 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import { UserRole } from "@/generated/prisma";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
 
 export class UsersController {
   create = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.json({ message: "ok" });
+      const bodySchema = z.object({
+        name: z.string().trim().min(2, { message: "Nome é obrigatório " }),
+        email: z.string().trim().toLowerCase(),
+        password: z.string().min(6),
+        role: z
+          .enum([UserRole.employee, UserRole.manager])
+          .default(UserRole.employee),
+      });
+
+      const { name, email, password, role } = bodySchema.parse(req.body);
+      res.json({ name, email, password, role });
     } catch (error) {
       next(error);
     }
